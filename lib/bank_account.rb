93b55173bbfc35524767ff492214acbe718b522b
sdonohue0918 <sdonohue0918@gmail.com>
2020-08-28 12:44:57 -0400

    All rspec passing for OO Banking Lab. Worked on by JessS and SeanD

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @balance = 1000
+        @name = name
+        @status = "open"
+    end
+
+    def deposit(deposit)
+        self.balance += deposit
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.balance > 0 && self.status == "open"
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,53 @@
 class Transfer
   # your code here
+    attr_accessor :sender, :receiver, :status, :amount
+    # @@transactions = []
+    
+    def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @status ='pending'
+      @amount = amount
+    end
+
+    def valid?
+      self.sender.valid? && self.receiver.valid?
+    end
+
+
+    def execute_transaction
+    
+        if valid? && self.sender.balance >= self.amount && self.status =='pending'
+              self.receiver.balance += self.amount
+              self.sender.balance -= self.amount
+              self.status = 'complete'
+              # @@transactions << self
+              #  binding.pry
+        else
+          self.status = 'rejected'
+          "Transaction rejected. Please check your account balance."
+        
+
+        end
+         
+    
+      end
+
+    def reverse_transfer
+        if self.status == 'complete'
+          self.receiver.balance -= self.amount
+          self.sender.balance += self.amount
+          self.status = 'reversed'
+       end
+
+        
+
+    
+    end
+
+    # def self.transactions
+    #   @@transactions
+    # end
+
 end
+
